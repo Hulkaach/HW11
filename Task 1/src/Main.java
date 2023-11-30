3c61b7cab1ba8ad360cf12dfc6970e3db6fb8fcc
DESKTOP-QQACB3T\Chester <zolinanata2006@mail.ru>
2023-11-30 22:28:38 +0300

    1

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Phone philips = new Phone();
+        Phone motorolla = new Phone(1091346711,"e398",123.44);
+        Phone xiaomi = new Phone(1234567890, "m6",100.1);
+        Phone nokia = new Phone(12367890,"3310",123.44);
+
+        nokia.receiveCall("Mother");
+        motorolla.receiveCall("Father",1231451);
+        xiaomi.receiveCall("Work");
 
     }
 }
